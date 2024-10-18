a96ec5f0644f1f0a1fd485bc002b174f5c7e0d1e
Mylena Silva <2214212@aluno.univesp.br>
2024-10-18 16:04:56 -0300

    update mock

--- pytest/mock.py
+++ pytest/mock.py
@@ -1,7 +1,7 @@
-import requests
-import pytest
-
-def test_get_data_from_api()
+def test_get_data_from_api():
     with requests_mock.Mocker() as mock:
-    data = get_data_from_api(api_url)
-  assert data == mocked_response
+        mock.register_uri('GET', api_url, json=mocked_response)
+        
+        response = get_data_from_api(api_url)
+        
+        assert response == mocked_response
